de1a43999c584263bbd4bfc1bf0930951bf10ce3
ahs1n <ahsanpervez74@gmail.com>
2021-08-27 15:48:22 +0500

    Add Sections

---
+++ app/src/main/java/edu/aku/hassannaqvi/epi_register/ui/sections/SectionCRActivity.java
@@ -0,0 +1,14 @@
+package edu.aku.hassannaqvi.epi_register.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SectionCRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_cr);
+    }
+}
---
+++ app/src/main/java/edu/aku/hassannaqvi/epi_register/ui/sections/SectionWRActivity.java
@@ -0,0 +1,14 @@
+package edu.aku.hassannaqvi.epi_register.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SectionWRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_wr);
+    }
+}
